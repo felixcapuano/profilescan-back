5f55ac808427c5a66203edafdd3654fdf677cd0e
felixcapuano <felix.capuano@gmail.com>
2022-04-21 19:45:48 +0200

    public dir

--- entrypoint.js
+++ entrypoint.js
@@ -13,9 +13,9 @@ if (process.env.NODE_ENV === "development") {
 app.use(require("./routes"));
 
 if (process.env.NODE_ENV !== "development") {
-  app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "public")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
   });
 }
 
